c5cd548b4da5bb7eba7c1f82a433cfd8e84ef32c
Andrew Adcock <aadcock@unleashed-technologies.com>
2018-11-04 14:06:24 -0500

    Set up base plugin
    Add plugin index file, environment checker, and core Passworder class.

---
+++ inc/core/Passworder.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * The core of the plugin
+ *
+ * Functionality needed to set up pluugin.
+ *
+ * @since       0.0.1
+ *
+ * @package Passworder
+ * @subpackage Passworder/inc/core/
+ */
+
+class Passworder {
+
+    public function __construct()
+    {
+
+    }
+
+    public function init()
+    {
+        
+    }
+}
